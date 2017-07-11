2722d22391ef28e04b85c9bdab1ff69a2758b3b1
Tiago Ferreira <me@tiferrei.com>
2017-07-11 14:49:55 +0100

    Added custom options

--- FAIC.cpp
+++ FAIC.cpp
@@ -32,18 +32,18 @@ class MyPrinter : public MatchFinder::MatchCallback {
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
-static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+static llvm::cl::OptionCategory FAICCategory("FAIC Options");
 
 // CommonOptionsParser declares HelpMessage with a description of the common
 // command-line options related to the compilation database and input files.
 // It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+static cl::extrahelp FAICHelp(CommonOptionsParser::HelpMessage);
 
 // A help message for this specific tool can be added afterwards.
 static cl::extrahelp MoreHelp("\nMore help text...");
 
 int main(int argc, const char **argv) {
-  CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+  CommonOptionsParser OptionsParser(argc, argv, FAICCategory);
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
 
